8e6027b5d1cc3368f87d8b4e78b4f47d4806ca67
Everson Mariano <mariano.computacao@gmail.com>
2024-01-12 18:36:29 -0300

    first commit

---
+++ src/main/java/com/everson/estacionaaqui/EstacionaAquiApplication.java
@@ -0,0 +1,13 @@
+package com.everson.estacionaaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionaAquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionaAquiApplication.class, args);
+	}
+
+}
